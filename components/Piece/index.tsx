80493c26003d708bd0d0f36cc31719633509a483
Andrea Alunni Guiducci <andrea.aguiducci@gmail.com>
2023-06-13 09:47:11 +0200

    limit row logic

--- components/Piece/index.tsx
+++ components/Piece/index.tsx
@@ -4,32 +4,69 @@ import Draggable from "react-native-draggable";
 import { useAppDispatch, useAppSelector } from "../../customHooks/reduxHooks";
 import { movePiece } from "../../store/slices/boardSlice";
 import { pieceColor } from "../../utils/utils";
+import { useState } from "react";
 
 export const Piece = ({ isDark, isKing, position }: any) => {
   const dispatch = useAppDispatch();
   const pieceMeasure = 25;
   const bgColor = isDark || isKing ? darkPiece : lightPiece;
   const board = useAppSelector((state) => state.board.board);
+  const [reverse, setReverse] = useState(false);
+  const [limit, setLimit] = useState();
 
-  const move = (oldPosition: any, newPosition: any) => {
-    const oldRow = oldPosition.row;
-    const oldCol = oldPosition.col;
-    const newRow = oldRow + Math.trunc(newPosition.y / 25);
-    const newCol = oldCol + Math.trunc(newPosition.x / 25);
-
+  const takePiece = () => {
     const newBoard = board.map((col) =>
       col.map((row: any) => {
-        if (oldCol === row.col && oldRow === row.row) {
+        if (position.row === row.row && position.col === row.col) {
           return { ...row, piece: null };
-        }
-        if (newCol === row.col && newRow === row.row) {
-          return { ...row, piece: isDark ? pieceColor.dark : pieceColor.light };
         }
         return row;
       })
     );
-    dispatch(movePiece(newBoard));
-    console.log(newBoard);
+    return newBoard;
+  };
+
+  const move = (oldPosition: any, newPosition: any) => {
+    const oldRow = oldPosition.row;
+    const oldCol = oldPosition.col;
+    const newRow = oldRow + Math.trunc(newPosition.y / pieceMeasure);
+    const newCol = oldCol + Math.trunc(newPosition.x / pieceMeasure);
+
+    if (oldRow === newRow) {
+      const rowToMove = takePiece()
+        .map((col) =>
+          col.filter((row: any) => oldRow === row.row)
+        ).map(box => box[0])
+        const startLImit = rowToMove.slice(0, oldCol).findLast((box:any) => box.piece)
+        const endLImit = rowToMove.slice(oldCol).find(box => box.piece)
+      debugger
+    }
+    if (
+      (newRow !== oldRow || newCol !== oldCol) &&
+      ((newRow === oldRow && newCol !== oldCol) ||
+        (newRow !== oldRow && newCol === oldCol))
+    ) {
+      const newBoard = board.map((col) =>
+        col.map((row: any) => {
+          if (oldCol === row.col && oldRow === row.row) {
+            return { ...row, piece: null };
+          }
+          if (newCol === row.col && newRow === row.row && !row.piece) {
+            return {
+              ...row,
+              piece: isKing
+                ? pieceColor.king
+                : isDark
+                ? pieceColor.dark
+                : pieceColor.light,
+            };
+          }
+          return row;
+        })
+      );
+      dispatch(movePiece(newBoard));
+    }
+    setReverse(true);
   };
 
   return (
@@ -44,6 +81,7 @@ export const Piece = ({ isDark, isKing, position }: any) => {
         onDragRelease={(e, gestureState) => {
           move(position, { x: gestureState.dx, y: gestureState.dy });
         }}
+        shouldReverse={reverse}
       />
     </View>
     // <Pressable
